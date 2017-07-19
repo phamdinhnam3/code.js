55326fa2aaded9692a8e77ee39d1b85ce305ca92
phamdinhnam3 <phamdinhnam3@gmail.com>
2017-07-19 16:01:08 +0700

    Update code.js

--- code.js
+++ code.js
@@ -1,2 +1,24 @@
-
-alert();
+if (window.location.href == "https://2captcha.com/cabinet/mobile"){
+	var sound = "http://david.guerrero.free.fr/Effects/WeaponHoming.wav";
+  
+	var id = document.getElementById("get_captcha");
+  var a = 0;
+	setInterval(function(){
+	  var str = id.innerHTML;
+	    
+		if(str.indexOf("Your account has been put into the sleeping mode.") != -1){
+			a=0;
+		}else
+		if(id.innerHTML == '<img src="/template/main/images/ajax-loader.gif">'){
+			a=0;
+		}else{
+		  if(a==0){
+  		  var audio = new Audio(sound);
+  		  audio.loop = false;
+  		  audio.play();
+  		  a=1;
+		  }
+		}
+		
+	},1000);
+}
